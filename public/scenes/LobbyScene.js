a4ff94d5248b200fb92de6bd91c1df8207129c81
kawillrich <66137285+kawillrich@users.noreply.github.com>
2024-04-27 20:18:13 -0400

    feat: add timer to lobbyScene to start initGame

--- public/scenes/LobbyScene.js
+++ public/scenes/LobbyScene.js
@@ -4,6 +4,17 @@ class LobbyScene extends Phaser.Scene {
     };
 
     create() {
-        this.add.text(20,20, "Lobby Screen");
+        this.add.text(20,20, "Lobby Screen").setOrigin(0,0);
+        let { height, width} = this.sys.game.canvas;
+        this.text = this.add.text(200, 250);
+        this.timedEvent = this.time.delayedCall(3000, this.initGame, [], this);
+    }
+
+    update () {
+        this.text.setText("Lobby populating players")
+    }
+
+    initGame() {
+        this.scene.start('initGame')
     }
 }
