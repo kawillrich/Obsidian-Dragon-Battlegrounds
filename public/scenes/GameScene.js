bf7f83a4e9738f6b925d72d4446a655995df9a6e
kawillrich <66137285+kawillrich@users.noreply.github.com>
2024-04-28 14:42:30 -0400

    format: cleaned up spacing

--- public/scenes/GameScene.js
+++ public/scenes/GameScene.js
@@ -1,48 +1,35 @@
 class GameScene extends Phaser.Scene {
     constructor() {
         super("initGame")
-    };
-
-    // preload() {
-        
-    // }
-
-    // create() {
-    //     this.add.text(20,20, "Game Screen");
-    // }
-
+    };  
     
-      preload() {
+    preload() {        
 
-        // this.load.spritesheet('heroAtlas', 'assets/images/hero48x48.png', {
-        //     frameWidth: 48,
-        //     frameHeight: 48
-        // })
+    this.load.atlas('heroAtlas', 'assets/images/hero48x48.png', 'assets/images/hero48x48.json');
 
-        this.load.atlas('heroAtlas', 'assets/images/hero48x48.png', 'assets/images/hero48x48.json');
-
-        this.load.image('bg', 'https://cdn.glitch.com/59aa1c5f-c16d-41a1-bfd2-09072e84a538%2Fbg.png?1551136995353');
-      }
-  
-      create() {
-        //add background
-        this.add.image(0,0,'bg')
-
-        //this.add.sprite(x, y, referenceImage, frameNumber)
-        this.hero = this.physics.add.sprite(200, 200, 'heroAtlas', "sprite1")
-
-        //add cursor keys
-        this.cursors = this.input.keyboard.createCursorKeys();
-
-
-
-        //add animations
-
-
-      }
-  
-      update() {
-      }
+    this.load.image('bg', 'https://cdn.glitch.com/59aa1c5f-c16d-41a1-bfd2-09072e84a538%2Fbg.png?1551136995353');
     }
+
+    create() {
+    //add background
+    this.add.image(0,0,'bg')
+
+    //this.add.sprite(x, y, referenceImage, frameNumber)
+    this.hero = this.physics.add.sprite(200, 200, 'heroAtlas', "sprite1")
+
+    //add cursor keys
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+
+
+    //add animations
+
+
+    }
+  
+    update() {
+    
+    }
+}
   
   
